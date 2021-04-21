7e46a8656f7231e79f82035e6543def5b2b1c366
RF604 <raffi@freedman.ca>
2021-04-21 12:36:49 -0700

    periodic commit

--- animation.js
+++ animation.js
@@ -1,3 +1,4 @@
+
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.intersectionRatio > 0.2) {
@@ -10,22 +11,7 @@ const observer = new IntersectionObserver(entries => {
   threshold: [0, 0.2, 0.8, 1]
 })
 
-// const images = document.querySelectorAll('img');
-// images.forEach(image => {
-//   observer.observe(image)
-// })
-
-const h1s = document.querySelectorAll('h1');
-h1s.forEach(h1 => {
-  observer.observe(h1)
-})
-
 const videos = document.querySelectorAll('video');
 videos.forEach(video => {
   observer.observe(video)
-})
-
-const miscfades = document.querySelectorAll('.miscfade');
-miscfades.forEach(miscfade => {
-  observer.observe(miscfade)
 })
